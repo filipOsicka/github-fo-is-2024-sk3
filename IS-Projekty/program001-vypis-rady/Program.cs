60e168f67b0d68ed9193400dd0b5e49b3533cfe0
Filip Osička <filip.osicka@tul.cz>
2024-10-03 17:49:08 +0200

    #1 prvni program - uzivatelsky vstup

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+    
+    static void Main() {
+        Console.Clear();
+        Console.WriteLine("***********************************");
+        Console.WriteLine("*********Výpis číselné řady********");
+        Console.WriteLine("***********Filip Osička************");
+        Console.WriteLine("***********************************");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // Vstup od uživatele
+        //Console.WriteLine("Zadejte prvni číslo: ");
+        //int first = int.Parse(Console.ReadLine());
+
+        // Vstup od uživatele   -   lépe řešený
+
+        Console.WriteLine("Zadejte prvni číslo: ");
+        int first;
+        while(!int.TryParse(Console.ReadLine(), out first)){
+            Console.WriteLine("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+        }
+
+        Console.ReadKey();
+
+
+    }
+}
