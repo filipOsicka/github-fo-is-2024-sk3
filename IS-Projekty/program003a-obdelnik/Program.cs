6dc82cac2456eac3dee8bf07a7cceec77a73495b
Filip Osička <filip.osicka@tul.cz>
2024-10-17 17:28:52 +0200

    #3 obdelnik - zakladni kod

---
+++ IS-Projekty/program003a-obdelnik/Program.cs
@@ -0,0 +1,30 @@
+﻿string again = "a";
+while(again == "a") {
+
+            Console.Clear();
+            Console.WriteLine("****************************");
+            Console.WriteLine("**** Výpis číselné řady ****");
+            Console.WriteLine("******* Tomáš Žižka ********");
+            Console.WriteLine("****************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            int first;
+            while(!int.TryParse(Console.ReadLine(), out first)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("========================================================");
+            Console.WriteLine("Zadané hodnoty: ");
+            Console.WriteLine("číslo: {0}", first);
+            Console.WriteLine("========================================================");
+          
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+            again = Console.ReadLine(); 
+
+}
