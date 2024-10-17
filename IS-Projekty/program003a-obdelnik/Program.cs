d3d2b02bd0d86c9fbb2c7b56902b3f679e3d2374
Filip Osička <filip.osicka@tul.cz>
2024-10-17 17:49:53 +0200

    #3 obdelnik - DONE

--- IS-Projekty/program003a-obdelnik/Program.cs
+++ IS-Projekty/program003a-obdelnik/Program.cs
@@ -3,24 +3,48 @@ while(again == "a") {
 
             Console.Clear();
             Console.WriteLine("****************************");
-            Console.WriteLine("**** Výpis číselné řady ****");
-            Console.WriteLine("******* Tomáš Žižka ********");
+            Console.WriteLine("********* Obdelník *********");
+            Console.WriteLine("******* Filip Osička *******");
             Console.WriteLine("****************************");
             Console.WriteLine();
             Console.WriteLine();
 
             Console.Write("Zadejte číslo (celé číslo): ");
-            int first;
-            while(!int.TryParse(Console.ReadLine(), out first)) {
+            uint width;
+            while(!uint.TryParse(Console.ReadLine(), out width)) {
+                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+            }
+
+            Console.Write("Zadejte číslo (celé číslo): ");
+            uint height;
+            while(!uint.TryParse(Console.ReadLine(), out height)) {
                 Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
             }
 
             Console.WriteLine();
             Console.WriteLine("========================================================");
             Console.WriteLine("Zadané hodnoty: ");
-            Console.WriteLine("číslo: {0}", first);
+            Console.WriteLine("Šířka: {0}", width);
+            Console.WriteLine("výška: {0}", height);
             Console.WriteLine("========================================================");
+
+            // for( int i = 1; i<=10 ;i++){
+            //     Console.WriteLine(i);
+            // }
+
+            // int j = 1;
+            // while (j <= 10) {
+            //     Console.WriteLine(j);
+            //     j++;
+            // }
           
+            for(int i = 1; i <= height ;i++){
+                for(int j = 1; j <= width ;j++){
+                   Console.Write("* "); 
+                   System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+                }
+                Console.WriteLine();
+            }
 
             Console.WriteLine();
             Console.WriteLine();
