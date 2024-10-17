f7e88b336e705e1205a82ff75ccf66bc1833e70c
Filip Osička <filip.osicka@tul.cz>
2024-10-17 17:22:32 +0200

    #2 soucet cifer - done

--- IS-Projekty/program002a-soucet-cifer/Program.cs
+++ IS-Projekty/program002a-soucet-cifer/Program.cs
@@ -22,24 +22,31 @@ while(again == "a") {
             Console.WriteLine("========================================================");
           
             int suma = 0;
+            int multi = 1;
             int numberBackup = number;
             int digit;
 
             if(number < 0){
                 number = -number;
             }
-            
+
             while(number >= 10) {
             digit = number % 10;
             number = (number-digit)/10;
             Console.WriteLine("Aktuální zbytek = {0}", digit);
             suma = suma + digit;
+            multi =  multi * digit;
+
             }
+
+
             
             Console.WriteLine("Aktuální zbytek = {0}", number);
             suma = suma + number;
+            multi =  multi * number;
 
             Console.WriteLine("\n\nSoučet cifer čísla {0} je {1}", numberBackup, suma);
+            Console.WriteLine("\n\nSoučin cifer čísla {0} je {1}", numberBackup, multi);
 
             Console.WriteLine();
             Console.WriteLine();
--- IS-Projekty/program002a-soucet-cifer/obj/Debug/net8.0/program002a-soucet-cifer.AssemblyInfo.cs
+++ IS-Projekty/program002a-soucet-cifer/obj/Debug/net8.0/program002a-soucet-cifer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cd88ecf19fd7812274832e5cfd019655196b585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e4b5ccfc9ca1eedf1f082a154e3f88f4f28312")]
 [assembly: System.Reflection.AssemblyProductAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program002a-soucet-cifer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
