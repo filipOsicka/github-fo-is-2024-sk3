e2c3c7016a550e3dad0d90eeca16c70d0d563085
Filip Osička <filip.osicka@tul.cz>
2024-11-21 13:10:17 +0100

    #11 vypocet pi - final

--- IS-Projekty/program011a-vypocet-pi/Program.cs
+++ IS-Projekty/program011a-vypocet-pi/Program.cs
@@ -9,16 +9,37 @@ while(again == "a") {
             Console.WriteLine();
             Console.WriteLine();
 
-            Console.Write("Zadejte číslo (celé číslo): ");
-            int first;
-            while(!int.TryParse(Console.ReadLine(), out first)) {
-                Console.Write("Nezadali jste celé číslo. Zadejte první číslo znovu: ");
+            Console.Write("Zadejte přesnost (reálné číslo - čím menší, tím přesnější výpočet bude): ");
+            double presnost;
+            while(!double.TryParse(Console.ReadLine(), out presnost)) {
+                Console.Write("Nezadali jste přesnost správně. Zadejte znovu: ");
             }
+
+            
+            double i = 1;
+            double piCtvrt = 1;
+            double znamenko = 1;
+            
+
+            while((1/i)>= presnost){
+                i=i+2;
+                znamenko = -znamenko;
+                piCtvrt = piCtvrt + znamenko *1/i;
+                if(znamenko ==1){
+                    Console.WriteLine("Zlomek +1/{0}; aktuální hodnota čísla PI = {1}", i, piCtvrt*4);
+                    
+                }else
+                    Console.WriteLine("Zlomek -1/{0}; aktuální hodnota čísla PI = {1}", i, piCtvrt*4);
+
+            }
+
+
+            
 
             Console.WriteLine();
             Console.WriteLine("========================================================");
-            Console.WriteLine("Zadané hodnoty: ");
-            Console.WriteLine("číslo: {0}", first);
+            Console.WriteLine("Hodnota čísla PI = {0}", piCtvrt*4);
+            Console.WriteLine("Hodnota čísla PI = {0:f4}", piCtvrt*4);
             Console.WriteLine("========================================================");
           
 
