f8faa31d89f7f28a7e9498766344662fb63054dd
Filip Osička <filip.osicka@tul.cz>
2024-11-28 10:05:08 +0100

    #13 aritmeticka posloupnost

---
+++ IS-Projekty/program013a-aritmeticka-posloupnost/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Zadejte počet čísel: ");
+            int pocetCisel = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadejte diferenci: ");
+            int diference = int.Parse(Console.ReadLine());
+
+            Console.Write("Zadejte první prvek posloupnosti: ");
+            int prvniPrvek = int.Parse(Console.ReadLine());
+
+            int soucet = 0;
+
+            Console.Write("Aritmetická posloupnost: ");
+            for (int i = 0; i < pocetCisel; i++)
+            {
+                int clenenPosloupnosti = prvniPrvek + i * diference;
+                Console.Write(clenenPosloupnosti + "; ");
+                soucet += clenenPosloupnosti; 
+            }
+
+            // Výstup součtu
+            Console.WriteLine();
+            Console.WriteLine("Součet zobrazených hodnot: " + soucet);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Chyba: Zadejte platná čísla.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Nastala chyba: " + ex.Message);
+        }
+
+        // Zajištění, že konzole zůstane otevřená
+        Console.WriteLine("Stiskněte libovolnou klávesu pro ukončení...");
+        Console.ReadKey();
+    }
+}
