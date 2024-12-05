2d87185b00880d612d6a4f8e002d63300592693b
Filip Osička <filip.osicka@tul.cz>
2024-12-05 13:30:49 +0100

    #16 NSD-NSN - final

--- IS-Projekty/program016a-NSD-NSN/Program.cs
+++ IS-Projekty/program016a-NSD-NSN/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    razitko();
+
+    ulong a = ziskatCislo("zadejte přirozené číslo a: ");
+    ulong b = ziskatCislo("zadejte přirozené číslo b: ");
+    
+    ulong nsd = vypocitatNsd(a, b);
+
+    ulong nsn = ziskatNSN(a,b,nsd);
+
+    zobrazitVysledky(a,b,nsn,nsd);
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("Program můžete opakovat stiskem klávesy 'a'");
+    again = Console.ReadLine();
+
+}
+
+static void razitko(){
+    Console.WriteLine("****************************");
+    Console.WriteLine("******** NSD a NSN *********");
+    Console.WriteLine("******* Filip Osička *******");
+    Console.WriteLine("****************************\n\n");
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+static ulong ziskatCislo(string zprava){
+    Console.Write(zprava);
+    ulong cislo;
+    while (!ulong.TryParse(Console.ReadLine(), out cislo))
+    {
+        Console.Write("Nezadali jste přirozené číslo. Zadejte znovu: ");
+    }
+    return cislo;
+}
+
+static ulong vypocitatNsd(ulong a, ulong b){
+    while (a != b){
+        if (a > b)
+        a = a - b;
+        else b = b - a;
+    }
+
+    return a;
+}
+
+static ulong ziskatNSN(ulong a, ulong b, ulong nsd){
+    
+    return a*b/nsd;
+}
+
+static void zobrazitVysledky(ulong a, ulong b, ulong nsn, ulong nsd){
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    Console.WriteLine("\n\nNSD čísel {0} a {1} je {2}", a, b, nsd);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"NSN čísel {a} a {b} je {nsn}");
+    Console.ForegroundColor = ConsoleColor.White;
+}
